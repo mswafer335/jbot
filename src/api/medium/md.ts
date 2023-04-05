4aa55e96551545874e8e1586979a365ecfe22e72
mallvielfrass <jack@bykollab.com>
2023-04-05 10:42:42 +0300

    part of medium

---
+++ src/api/medium/md.ts
@@ -0,0 +1,11 @@
+export class mediumApi {
+    token: string;
+    url: string;
+    constructor(url: string, token: string) {
+        this.url = url;
+        this.token = token;
+    }
+    // private request(method: string, url: string, data: any, headers: any) {
+    //     axios;
+    // }
+}
