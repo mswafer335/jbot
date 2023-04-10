0ca0ca33c2127823d91a3bec4456bc436e2d5ca9
Jack <jack@bykollab.com>
2023-04-10 11:58:52 +0300

    add check credential

--- src/api/twitter/twi.ts
+++ src/api/twitter/twi.ts
@@ -10,7 +10,26 @@ export class TwitterApi {
             access_token_secret: access_token_secret,
             timeout_ms: 60 * 1000,
             strictSSL: true,
+            app_only_auth: true,
         });
+    }
+    async verifyCredentials() {
+        // Twit has promise support; you can use the callback API,
+        // promise API, or both at the same time.
+        //
+        this.T.get("account/verify_credentials", { skip_status: true })
+            .catch(function (err) {
+                console.log("caught error", err.stack);
+            })
+            .then(function (result) {
+                // `result` is an Object with keys "data" and "resp".
+                // `data` and `resp` are the same objects as the ones passed
+                // to the callback.
+                // See https://github.com/ttezel/twit#tgetpath-params-callback
+                // for details.
+
+                console.log("data", result?.data);
+            });
     }
     // Получение списка подписчиков
     async getFollowers(user: string) {
