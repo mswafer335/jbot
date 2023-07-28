22d25e5694d9c1a5f26289e7ffdfb140acdeeb2b
mallvielfrass <jack@bykollab.com>
2023-07-28 08:06:19 +0300

    handle server adress

--- src/const.ts
+++ src/const.ts
@@ -5,10 +5,10 @@ console.log(`Bot notifyPeriod: ${notifyPeriod}`);
 dotenv.config();
 //console.log(process.env);
 export const port = process.env.PORT;
-export const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
+
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
-export const REDIRECT_URL = process.env.REDIRECT_URL;
+
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 export const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
@@ -73,11 +73,6 @@ if (!TWITTER_CONSUMER_SECRET) {
     process.exit(1);
 }
 
-export const TWITTER_CALLBACK_URL = process.env.TWITTER_CALLBACK_URL!;
-if (!TWITTER_CALLBACK_URL) {
-    console.error("Run: TWITTER_CALLBACK_URL not found");
-    process.exit(1);
-}
 //TWITTER_CHANNEL_USERNAME
 export const TWITTER_CHANNEL_USERNAME = process.env.TWITTER_CHANNEL_USERNAME!;
 if (!TWITTER_CHANNEL_USERNAME) {
@@ -91,3 +86,17 @@ if (!SECRET_FOR_ENCODE_TGID) {
     console.error("Run: SECRET_FOR_ENCODE_TGID not found");
     process.exit(1);
 }
+//server address
+
+export let SERVER_ADDRESS = process.env.SERVER_ADDRESS;
+if (!SERVER_ADDRESS || SERVER_ADDRESS == "") {
+    console.error("Run: SERVER_ADDRESS not found");
+    process.exit(1);
+} else {
+    if (SERVER_ADDRESS.endsWith("/")) {
+        SERVER_ADDRESS = SERVER_ADDRESS.replace(/\/$/, "");
+    }
+}
+
+export const REDIRECT_URL = SERVER_ADDRESS;
+export const TWITTER_CALLBACK_URL = SERVER_ADDRESS + "/";
